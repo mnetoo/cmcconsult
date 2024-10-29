484ba0de5861152ce282d34d38d9784eb986412d
Marcus Neto <marcusnetoo@outlook.com>
2024-10-29 12:47:54 -0300

    desenvolvimento

---
+++ javascript/index.js
@@ -0,0 +1,25 @@
+function redirectToPage() {
+    window.open("checklist/inicio.html", "_blank"); 
+}
+
+function redirectToPage2() {
+    window.open("cadastro.html", "_blank"); 
+}
+
+function redirectToPage3() {
+    window.open("senhas.html", "_blank"); 
+}
+
+function redirectToPage4() {
+    window.open("links.html", "_blank"); 
+}
+
+function increaseSize(element) {
+    element.classList.add("aumentar"); // Adiciona a classe para aumentar apenas a div específica
+}
+
+function resetSize(element) {
+    element.classList.remove("aumentar"); // Remove a classe para retornar ao tamanho original
+}
+
+/*==================================================================================================================*/
---
+++ javascript/inicio.js
@@ -0,0 +1,23 @@
+function redirectToPagePP() {
+    redirectToPage("checklist/portPublico.html");
+}
+
+function redirectToPageNP() {
+    redirectToPage("checklist/novoPublico.html");
+}
+
+function redirectToPagePI() {
+    redirectToPage("checklist/portINSS.html");
+}
+
+function redirectToPageNI() {
+    redirectToPage("checklist/novoINSS.html");
+}
+
+function redirectToPageLS() {
+    redirectToPage("checklist/liquidSimultanea.html");
+}
+
+function redirectToPageCD() {
+    redirectToPage("checklist/compraDivida.html");
+}
